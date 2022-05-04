3d5f6751fb11da597b718d8ab68f9a4016ed2652
TheShinyM <shinmow4@gmail.com>
2022-05-04 20:52:33 +0200

    bessere Version der Webseite

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,35 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: "app-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-  public open: boolean = true;
+    config: any;
+    fullpage_api: any;
 
-  constructor() { }
+    public open: boolean = true;
 
-  ngOnInit(): void {
-  }
+    constructor() {
+        this.config = {
+            // fullpage options
+            licenseKey: "E8B99B72-16C84745-96B3A6AE-817347D7",
+            anchors: ["intro", "contact"],
+            menu: "#menu",
 
-  public openMenu(): void{
-    if (this.open === true) {
-        // document.getElementById("menus").style.display = "block";
-        document.getElementById("menus").style.transform = "translateY(0px)";
-        document.getElementById("menus").style.opacity = "1";
-        document.getElementById("menus").style.transition = "opacity 450ms ease-in-out, transform 450ms ease-in-out";
-        document.getElementById("menus").style.pointerEvents = "auto";
-        this.open = false;
-    } else if (this.open == false) {
-        // document.getElementById("menus").style.display = "none";
-
-        document.getElementById("menus").style.pointerEvents = "none";
-        document.getElementById("menus").style.transition = "opacity 450ms ease-in-out, transform 450ms ease-in-out";
-        document.getElementById("menus").style.opacity = "0";
-        document.getElementById("menus").style.transform = "translateY(-20px)";
-
-        this.open = true;
+            // fullpage callbacks
+            afterResize: () => {
+                console.log("After resize");
+            }
+        };
     }
-  }
+
+    ngOnInit(): void {}
+
+    public getRef(fullPageRef: any): void {
+        this.fullpage_api = fullPageRef;
+    }
+
+    public openMenu(): void {
+        if (this.open === true) {
+            // document.getElementById("menus").style.display = "block";
+            document.getElementById("menus").style.transform = "translateY(0px)";
+            document.getElementById("menus").style.opacity = "1";
+            document.getElementById("menus").style.transition =
+                "opacity 450ms ease-in-out, transform 450ms ease-in-out";
+            document.getElementById("menus").style.pointerEvents = "auto";
+            this.open = false;
+        } else if (this.open == false) {
+            // document.getElementById("menus").style.display = "none";
+
+            document.getElementById("menus").style.pointerEvents = "none";
+            document.getElementById("menus").style.transition =
+                "opacity 450ms ease-in-out, transform 450ms ease-in-out";
+            document.getElementById("menus").style.opacity = "0";
+            document.getElementById("menus").style.transform = "translateY(-20px)";
+
+            this.open = true;
+        }
+    }
 }
